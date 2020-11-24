98d635035827764670d3cf39a36721c7d4ab71f1
yangmr <759168442@qq.com>
2020-11-24 12:13:12 +0800

    配置vue.config.js

--- 4day/blog-admin/.eslintrc.js
+++ 4day/blog-admin/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     parser: "babel-eslint"
   },
   rules: {
+    semi: [1, "never"], // 禁止用分号
+    indent: [2, 2], // 缩进采用2个空格
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
--- 4day/blog-admin/vue.config.js
+++ 4day/blog-admin/vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    publicPath : "./"
+  //设置项目打包时的路径
+  publicPath: "./",
+  //开发服务器 / 脚手架服务器
+  devServer: {
+    port: 9999, //端口号
+    open: true, //项目启动完成时自动打开浏览器加载项目
+    host: "localhost", //主机名
+    https: false //是否开启https协议
+  },
+  //关闭eslint
+  lintOnSave: false,
+  // 打包时, 不生成 .map 文件, 加快打包构建
+  productionSourceMap: false,
 }
